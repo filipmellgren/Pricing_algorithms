c5a92ce41e671556eb0d092c921baf69ff707fa4
filipmellgren <filip.mellgren@gmail.com>
2019-12-03 22:28:55 +0100

    My commit

---
+++ Q_main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 19:54:30 2019
+
+@author: filip
+"""
+## Libraries
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import math
+#import environment as env
+from bertrand_nash import BertrandNash
+import gym
+import random
+
+#### Load environment and Q table structure
+env = BertrandNash() # read environment into this
+# env.observation_space.n,env.action_space.n
+#Q = np.zeros((env.state_size, env.action_size)) # Q table
+Q = np.zeros((100, 100)) # Q table
+##### Parameters
+gamma = 0.99 # discount factor
+alpha = 0.1 # learning rate. Should be decreasing over iterations
+epsilon = 0.5 # Should also decrease
+epsilon_decay = 0.999
+#reward = profit(state, action)# defined as profit in period t. Actually a table?
+num_episodes = 100
+num_steps = 99
+
+
+#### Q learning Algorithm
+# loop over training episodes p.105 Sutton
+for i_episode in range(num_episodes):
+    # Set initial observation
+    S = env.reset()
+    # loop for each step of the episode until S is a terminal state (my case, after convergence)
+    for t in range(num_steps):
+        env.render() # ?
+        # Action choice:
+        if random.uniform(0, 1) < epsilon:
+            A = env.action_space.sample() # Explore action space
+        else:
+            A = np.argmax(Q[S]) # Exploit learned values
+        A = np.asscalar(A)
+        # Take action A, observe R, S_next
+        S_next, R, done, info = env.step(A) # env.step(action)
+        # Update Q table:
+        Q[S, A] =  Q[S, A] + alpha *(R + gamma * np.max(Q[S_next, :])- Q[S, A])
+        # If S_next is terminal: set value funciton target to R
+        # Move to next stage:
+        S = S_next
+env.close()
+        
+      
+Q = np.zeros((2, 4))
---
+++ bertrand_nash.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 12:07:10 2019
+
+@author: filip
+"""
+
+import gym
+from gym import spaces
+import numpy as np
+import random
+
+class BertrandNash(gym.Env): # give it the Box environment
+  """Custom Environment that follows gym interface
+      This class describes a market characterised by Bertrand behaviour
+      
+      Demand is stochastic:
+          prob alpha, zero demand
+          prob (1-alpha), high demand state
+
+      https://stackoverflow.com/questions/52727233/how-can-i-register-a-custom-environment-in-openais-gym
+  """
+  metadata = {'render.modes': ['human']} #?
+
+  def __init__(self):
+    # Define action and observation space
+    # They must be gym.spaces objects
+    
+    # inherit class methods from another gym environment
+    # Initialize object with values here
+    # Actions are setting a price inside the interval
+    #self.action_space = spaces.Box(
+    #        low=np.array([0, 0]), high=np.array([1, 1]), dtype=np.float16) # can be of form: np.array([0, 0]). TODO: High should be infinity!
+    # An instance of something else (Box environment)
+  #  self.observation_space = spaces.Box() # What can we observe? Demand function?
+        # Actions of the format Buy x%, Sell x%, Hold, etc.
+    self.action_space = spaces.Box(low=0, high=99, shape=(1, 1), dtype=np.int_)
+    
+    # Prices contains the OHCL values for the last five prices
+    self.observation_space = spaces.Box(
+      low=0, high=1, shape=(6, 6), dtype=np.float16)
+    
+    self.state_size = 1
+    self.profit = 0
+
+    
+    super().__init__()
+#    super().all(*args, **kwargs)
+    
+  def step(self, action):
+      # Execute one time step within the environment
+      self._take_action(action)
+      self.current_step += 1
+      reward = self.profit 
+      done = self.current_step > 100
+      obs = self._next_observation()
+      return obs, reward, done, {}
+    
+  def reset(self):
+      # Reset the state of the environment to an initial state
+      self.acc_profit = 0
+      self.current_step = 0 # ??
+      return self._next_observation()
+  
+  def _next_observation(self):
+      #obs = np.array([self.profit])
+      obs = self.current_step
+      return obs
+
+  def _take_action(self, action):
+      # Here is where the agent decides how much to charge
+      self.demand = 100 - action
+      self.profit = self.demand * action
+      self.acc_profit += self.profit
+        
+  def render(self, mode='human', close=False):
+      # Render the environment to the screen
+      print(f'Step: {self.current_step}')
+      print(f'Profit: {self.profit}')
+      print(f'Acc Profit: {self.acc_profit}')
