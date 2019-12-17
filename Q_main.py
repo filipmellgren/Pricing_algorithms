747e6ca32005fd27eb31c0a069aad8effe973937
filipmellgren <filip.mellgren@gmail.com>
2019-12-17 21:48:51 +0100

    It runs but results are random

--- Q_main.py
+++ Q_main.py
@@ -33,7 +33,7 @@ from config import PARAMS
 env = PARAMS[0]
 GAMMA = PARAMS[1]
 ALPHA = PARAMS[2]
-EPSILON = PARAMS[3]
+BETA = PARAMS[3]
 NUM_EPISODES = PARAMS[4]
 
 # Objects
@@ -45,45 +45,49 @@ writer = SummaryWriter(comment="-q-iteration")
 iter_no = 0
 
 ## TODOs ##
-# Run  function with more natural parameterizations, like when to stop.
-# When to break an episode
-#   break after one billion (high number) or after convergence
-# Define convergence and a target goal
-# How is the observation spce updated? how dows it work?
-    # maybe in value_update. 
+# Replicate Calvano:
+    # update explore exploit schedule
+    # Update when to break an episode (define convergence)
+    # Initialize the Q table in a smarter way. P. 8 Calvano 
+
+# Continuous case
     # Make the observation space a box so that I can add on DL
-# initialize the Q table in a smarter way. P. 8 Calvano
-# Calvano et al introduces a bounded memeory of k steps which defines the state
-    # they use k = 1
-# Also introduce their greedy policy. Then I might be able to replicate their findings
-# Make one discrete environment and one continuous. Start with discrete for simplicity.
-    # Replicate Calvano and make sure that results are similar. Then try new things
-    # such as DQN, new demand function etc.
+   
+# Other
+    # Calvano et al introduces a bounded memeory of k steps which defines the state
+        # they use k = 1
+    # Try new things: 
+        # such as DQN, new demand function etc.
 
 #### Q learning Algorithm
 # loop over training episodes p.105 Sutton
+rew = np.zeros((100002,NUM_EPISODES))
 for ep in range(NUM_EPISODES):
     # 1: initialise Qs
     env.reset()
+    iter_no = 0
+    s_next = 0
     while True: # number of steps per episode
         iter_no += 1
+        eps = 1 - np.exp(-BETA * (iter_no))
         # 2: agents choose actions simultanously. 
-        action1 = agent1.act() # does this perhaps need to take eps as arg?
-        action2 = agent2.act()
+        action1 = agent1.act(eps)
+        action2 = agent2.act(eps)
         action = action1*10 + action2 # 10 hardcoded temporary. it's ncol, number of discrete actions
         # 3: outcomes are calculated
-        s, reward_n, done, prob = env.step(action)        
+        s = s_next # remember old state
+        s_next, reward_n, done, prob = env.step(action)        
         #s, a, r, s_next = env.interact(np.asarray([action1, action2]))
-        s_next = action
         # 4: Bellman updates
         agent1.value_update(s, action1 ,reward_n[0],s_next)
         agent2.value_update(s, action2 ,reward_n[1],s_next)
         #agent1.value_update(s[0].item(), a[0].item() ,r[0].item(), s_next[0].item())
         #agent2.value_update(s[1].item(), a[1].item(), r[1].item(), s_next[1].item())
         # 5: repeat until convergence
-        if iter_no > 1000:
+        rew[iter_no][ep] = reward_n[0]
+        if iter_no > 100000: # Calvano takes it up to a billion or until convergence!
             break
+
+
 env.close()
-
-
 
