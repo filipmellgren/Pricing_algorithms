48182149409a5895bf96e0ab88f4599ea0479266
filipmellgren <filip.mellgren@gmail.com>
2019-12-17 17:13:33 +0100

    Discrete env now runs and is built on top of discrete.Discrete(). Next, check if it does what's intended

--- Q_main.py
+++ Q_main.py
@@ -3,43 +3,38 @@
 """
 Created on Sun Dec  1 19:54:30 2019
 
-@author: filip
-
+@author: Filip Mellgren
 
 !git add "Q_main.py"
 !git add "agents.py"
 !git add "bertrand_nash.py"
-!git add "bertrand_nash.py"
+!git add "prisoner_dilemma.py"
+!git add "config.py"
 !git commit -m "My_commit"
 !git push origin master
+
+Two Q-tables are necessary, one for each competitor
+This makes the environment "non stationary" – i.e. hard to solve and not guaranteed to converge
+Approach taken: so called "independent learning". Agents don't necessarily know they are competing, they simply observe profits for various prices
+Need Deep learning to approximate neighbouring cells
+Important difference between this and Calvano's paper is what's observable. 
+     In this case, players observe their own profit and never the opponents price.
 """
-## Libraries
+# Libraries
 import numpy as np
-import random
 import matplotlib.pyplot as plt
-#import environment as env
-from bertrand_nash import BertrandNash
 import gym
 import collections
 from tensorboardX import SummaryWriter
-from agents import Agent
-from config import PARAMS
-#### Load environment and Q table structure
-# Two Q-tables are necessary, one for each competitor
-# This makes the environment "non stationary" – i.e. hard to solve and not guaranteed to converge
-# Approach taken: so called "independent learning". Agents don't necessarily know they are competing, they simply observe profits for various prices
-# Need Deep learning to approximate neighbouring cells
-# Important difference between this and Calvano's paper is what's observable. 
-    # In this case, players observe their own profit and never the opponents price.
-    
-# TODO: initialize the Q table in a smarter way. P. 8 Calvano
-# Parameters
+from agents import Agent  
 
+# Parameters
+from config import PARAMS
 env = PARAMS[0]
 GAMMA = PARAMS[1]
 ALPHA = PARAMS[2]
 EPSILON = PARAMS[3]
-
+NUM_EPISODES = PARAMS[4]
 
 # Objects
 agent1 = Agent()
@@ -51,12 +46,19 @@ iter_no = 0
 
 ## TODOs ##
 # Run  function with more natural parameterizations, like when to stop.
-# Number of episodes, when to break an episode
+# When to break an episode
+#   break after one billion (high number) or after convergence
 # Define convergence and a target goal
-# How is the observation spce updated?
+# How is the observation spce updated? how dows it work?
     # maybe in value_update. 
-    # May want to start out simple and define discrete outcomes in the demand function
-    # And then make the observation space a box when I add on DL
+    # Make the observation space a box so that I can add on DL
+# initialize the Q table in a smarter way. P. 8 Calvano
+# Calvano et al introduces a bounded memeory of k steps which defines the state
+    # they use k = 1
+# Also introduce their greedy policy. Then I might be able to replicate their findings
+# Make one discrete environment and one continuous. Start with discrete for simplicity.
+    # Replicate Calvano and make sure that results are similar. Then try new things
+    # such as DQN, new demand function etc.
 
 #### Q learning Algorithm
 # loop over training episodes p.105 Sutton
@@ -68,14 +70,20 @@ for ep in range(NUM_EPISODES):
         # 2: agents choose actions simultanously. 
         action1 = agent1.act() # does this perhaps need to take eps as arg?
         action2 = agent2.act()
+        action = action1*10 + action2 # 10 hardcoded temporary. it's ncol, number of discrete actions
         # 3: outcomes are calculated
-        s, a, r, s_next = env.interact(np.asarray([action1, action2]))
+        s, reward_n, done, prob = env.step(action)        
+        #s, a, r, s_next = env.interact(np.asarray([action1, action2]))
+        s_next = action
         # 4: Bellman updates
-        agent1.value_update(s[0].item(), a[0].item() ,r[0].item(), s_next[0].item())
-        agent2.value_update(s[1].item(), a[1].item(), r[1].item(), s_next[1].item())
+        agent1.value_update(s, action1 ,reward_n[0],s_next)
+        agent2.value_update(s, action2 ,reward_n[1],s_next)
+        #agent1.value_update(s[0].item(), a[0].item() ,r[0].item(), s_next[0].item())
+        #agent2.value_update(s[1].item(), a[1].item(), r[1].item(), s_next[1].item())
         # 5: repeat until convergence
-        if iter_no > 100:
+        if iter_no > 1000:
             break
 env.close()
+
 
 
--- prisoner_dilemma.py
+++ prisoner_dilemma.py
@@ -19,7 +19,10 @@ class PrisonerDilemma(discrete.DiscreteEnv):  # maybe have to drop gym.Env
     # useful blog post:
     # https://stackoverflow.com/questions/52727233/how-can-i-register-a-custom-environment-in-openais-gym
   """
-  Similar to BertrandNash but simplified to represent a discrete scenario
+  Similar to BertrandNash but simplified to represent a discrete scenario.
+  
+  Similar to FrozenLake. Instead of a row and column coordinate,
+  we have one price for player 1, and another for player 2.
   
   Inherits from discrete.Discrete which:
       
@@ -40,99 +43,154 @@ class PrisonerDilemma(discrete.DiscreteEnv):  # maybe have to drop gym.Env
 
   def __init__(self):
       # self.nrow, self.ncol = nrow, ncol = desc.shape
-      nrow = 20
-      ncol = 1
-      nA = 4
+      nA = 10 # Number of unique prices
+      nrow = nA
+      ncol = nA
       nS = nrow * ncol
       isd = np.zeros(nS)
-      P = {s : {a : [] for a in range(nA)} for s in range(nS)}
+      P = {s : {a : [] for a in range(nA*nA)} for s in range(nS)} # Takes both actions into account
+      
+      def to_s(row, col):
+          '''
+          enumerates row, col combo to a state. the row and col can be thought
+          of as actions in the last period in this application
+          '''
+          return(row*ncol + col)
+          
+      def profit_n(action_n):
+          a0 = 1 
+          ai = 2
+          aj = 2
+          a = np.array([ai, aj])
+          a_not = np.flip(a) # to obtain the other firm's a
+          mu = 1
+          
+          p = action_n/10 # transforms action into price range
+          
+          p_not = np.flip(p) # to obtain the other firm's p
+          num = np.exp((a - p)/mu)
+          denom = np.exp((a - p)/(mu)) + np.exp((a_not - p_not)/(mu)) + np.exp(a0/mu)
+          quantity_n = num / denom
+          
+          profit = quantity_n * p # mc = 0
+          return(profit)
+          
+      for row in range(nrow):
+            for col in range(ncol):
+                s = to_s(row, col)
+                for action1 in range(nA): 
+                    for action2 in range(nA):
+                        a = to_s(action1, action2)
+                        action_n = np.array([action1, action2])
+                        li = P[s][a]
+                        newrow, newcol = action1, action2
+                        newstate = to_s(newrow, newcol)
+                        done = False
+                        reward_n = profit_n(action_n)
+                        done = False # Not sure hot to handle
+                        li.append((1.0, newstate, reward_n, done)) # 1.0 is probability
+      
       super(PrisonerDilemma, self).__init__(nS, nA, P, isd)
-        #    super().all(*args, **kwargs)
     
     # Markov game: 
     #https://stackoverflow.com/questions/44369938/openai-gym-environment-for-multi-agent-games
-x = np.random.randint(0,2, 10)
-y = np.random.randint(0,2, 10)
-np.array(x == y).astype('float64').ravel()
 
-  def step(self, action_n):
-      # agent gives action
-      # info about outcome is returned (next obs, reward, end flag)
-      # action_n..... list of actions taken by the agents
-      
-      # set action for each agent
-      '''
-      for i, agent in enumerate(self.agents):
-          self._take_action(action_n[i], agent)
-      ''' 
-      
-      self.demand(action_n) # sets self.quantity_n
-      self.profit_n = np.asarray(self.quantity_n) * np.asarray(action_n) #maybe: break out into its own method, like quantities
-      reward_n = self.profit_n
-      self.state_n = reward_n
-      done =  self.current_step > 100 # TODO: don't hardcode
-      #info_n   = {'n': []} # TODO: what is this?
-      self.current_step += 1
-      return self.state_n, reward_n, done, {}
-  # numpy matrix, float value of reward, boolean, extra info
-          
-  def reset(self):
-      # Reset the state of the environment to an initial state
-      self.current_step = 0
-      self.profit_n = np.asarray([0,0]) # TODO: Set up smarter
-      self.state_n = self.profit_n 
-      
-        
-  def render(self, mode='human', close=False):
-      # Render the environment to the screen
-      print(f'Step: {self.current_step}')
-      print(f'Profit: {self.profit}')
-      
 
   def interact(self, action_n):
       old_state_n = self.state_n
       new_state_n, reward_n, is_done, _ = self.step(action_n)
       self.state_n = self.reset() if is_done else new_state_n
       return(old_state_n, action_n, reward_n, new_state_n)
-      
-  def demand(self, action_n):
-      '''
-      demand() calculates demand for each agent's product given actions prices)
-        WORKS FOR 2 FIRMS. For n firms, denom is a longer sum for j = 1...n
-      INPUT:
-          action_n.......array of actions (prices) taken by the agents
-          quality........list with index of product qualities
-          mu.............index of horizontal differentiation
-      OUTPUT: 
-          quantities.....list of quantities, one for each agent. 
-      '''
-# TODO: add complexity once system is working
-      # OR, make it flexible within the Discrete environment
-      # Parameters used, might want to keep them at the top of the class, outside the function or pass as arguments
-      '''
-      a0 = 1 
-      ai = 2
-      aj = 2
-      mu = 1
-      a = np.array([ai, aj])
-      a_not = np.flip(a) # to obtain the other firm's a
-      p = action_n
-      p_not = np.flip(p) # to obtain the other firm's p
 
-      action_n
-      num = np.exp((a - p)/mu)
-      denom = np.exp((a - p)/(mu)) + np.exp((a_not - p_not)/(mu)) + np.exp(a0/mu)
-      self.quantity_n = num / denom
-      '''
-      p = action_n
-      if all(p == np.array([0,0])):
-          self.quantity_n =  np.array([0,0])
-      elif all(p == np.array([1,0])):
-          self.quantity_n =  np.array([0,2])
-      elif all(p == np.array([0,1])):
-          self.quantity_n =  np.array([2,0])
-      elif all(p == np.array([1,1])):
-          self.quantity = np.array([50,50])
-      else:
-          print(p)
-      return
+
+# =============================================================================
+# def to_s(row, col):
+#     '''
+#     method enumerates all the states in the frozen grid world
+#     I reallu just need a way to enumerate all the states as well
+#     '''
+#             return row*ncol + col
+# 
+#   def step(self, action_n):
+#       # agent gives action
+#       # info about outcome is returned (next obs, reward, end flag)
+#       # action_n..... list of actions taken by the agents
+#       
+#       # set action for each agent
+#       '''
+#       for i, agent in enumerate(self.agents):
+#           self._take_action(action_n[i], agent)
+#       ''' 
+#       
+#       self.demand(action_n) # sets self.quantity_n
+#       self.profit_n = np.asarray(self.quantity_n) * np.asarray(action_n) #maybe: break out into its own method, like quantities
+#       reward_n = self.profit_n
+#       self.state_n = reward_n
+#       done =  self.current_step > 100 # TODO: don't hardcode
+#       #info_n   = {'n': []} # TODO: what is this?
+#       self.current_step += 1
+#       return self.state_n, reward_n, done, {}
+#   # numpy matrix, float value of reward, boolean, extra info
+#           
+#   def reset(self):
+#       # Reset the state of the environment to an initial state
+#       self.current_step = 0
+#       self.profit_n = np.asarray([0,0]) # TODO: Set up smarter
+#       self.state_n = self.profit_n 
+#       
+#         
+#   def render(self, mode='human', close=False):
+#       # Render the environment to the screen
+#       print(f'Step: {self.current_step}')
+#       print(f'Profit: {self.profit}')
+#       
+# 
+#   def interact(self, action_n):
+#       old_state_n = self.state_n
+#       new_state_n, reward_n, is_done, _ = self.step(action_n)
+#       self.state_n = self.reset() if is_done else new_state_n
+#       return(old_state_n, action_n, reward_n, new_state_n)
+#       
+#   def demand(self, action_n):
+#       '''
+#       demand() calculates demand for each agent's product given actions prices)
+#         WORKS FOR 2 FIRMS. For n firms, denom is a longer sum for j = 1...n
+#       INPUT:
+#           action_n.......array of actions (prices) taken by the agents
+#           quality........list with index of product qualities
+#           mu.............index of horizontal differentiation
+#       OUTPUT: 
+#           quantities.....list of quantities, one for each agent. 
+#       '''
+# # TODO: add complexity once system is working
+#       # OR, make it flexible within the Discrete environment
+#       # Parameters used, might want to keep them at the top of the class, outside the function or pass as arguments
+#       '''
+#       a0 = 1 
+#       ai = 2
+#       aj = 2
+#       mu = 1
+#       a = np.array([ai, aj])
+#       a_not = np.flip(a) # to obtain the other firm's a
+#       p = action_n
+#       p_not = np.flip(p) # to obtain the other firm's p
+# 
+#       action_n
+#       num = np.exp((a - p)/mu)
+#       denom = np.exp((a - p)/(mu)) + np.exp((a_not - p_not)/(mu)) + np.exp(a0/mu)
+#       self.quantity_n = num / denom
+#       '''
+#       p = action_n
+#       if all(p == np.array([0,0])):
+#           self.quantity_n =  np.array([0,0])
+#       elif all(p == np.array([1,0])):
+#           self.quantity_n =  np.array([0,2])
+#       elif all(p == np.array([0,1])):
+#           self.quantity_n =  np.array([2,0])
+#       elif all(p == np.array([1,1])):
+#           self.quantity = np.array([50,50])
+#       else:
+#           print(p)
+#       return
+# =============================================================================
+
