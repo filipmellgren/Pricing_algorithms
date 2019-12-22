8314139106fad8f5df0335a594da6198a9134a38
filipmellgren <Filip.mellgren@gmail.com>
2019-12-22 10:47:51 +0100

    Adding new folder for new system

---
+++ disc_part_obs/Q_main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 10:46:51 2019
+
+@author: filip
+
+This is the main file for a multi agent reinforcement learning application
+where two agents compete in a discrete Bertrand environment, i.e. having price
+as their strategic variable. The agents are able to see their own prices and
+profits K periods back. They are unable to see each others prices and profits.
+
+Question is, can they still learn how to cooperate?
+
+!git add "Q_main2.py"
+!git add "agents.py"
+!git add "prisoner_dilemma.py"
+!git add "config.py"
+!git commit -m "My_commit"
+!git push origin master
+
+!git pull
+
+"""
+
