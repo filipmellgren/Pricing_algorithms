d9dbec710b9cf7befc056f2ffc1bcf6b8181e5fb
filipmellgren <filip.mellgren@gmail.com>
2019-12-25 18:52:31 +0100

    added code for nash and monopoly prices/profits

--- disc_part_obs/config2.py
+++ disc_part_obs/config2.py
@@ -5,8 +5,9 @@ Created on Sun Dec 22 10:55:38 2019
 
 @author: filip
 """
-
 import numpy as np
+
+# Parameters
 GAMMA = 0.95 # discounting factor
 ALPHA = 0.1 # learning rate
 BETA = 0.5*10**(-5) # Parameter for epsilon greedy approach. Lower leads to more exploration
@@ -18,38 +19,25 @@ ITER_BREAK = 10**5 # Calvano uses 10**9
 # But convergence requires a very large number
 # of periods, on the order of hundreds of thousands
 # =============================================================================
-
 CONV = 25000 # Calvano uses 25000
 K = 1 # Length of memory. I.e. remembering the last step
 M = 15 # n.o. actions.  Makes the discrete environment the prisoner dilemma (they mostly use 15)
+nA = M
 nS = M**K
 
 PARAMS = np.array([GAMMA, ALPHA, BETA, NUM_EPISODES, K, M, nS, ITER_BREAK, CONV])
 
+# Parameters and functions related to the economic environment:
+    # Given parameters
 C = 1
 A = 2
 A0 = 1
 MU = 1/2
-# TODO: derive these 
-MIN_PRICE = 1.6136136136136137
-MAX_PRICE = 1.7371274174174174
-price_range = MAX_PRICE- MIN_PRICE
-# TODO: derive analytically and more generally
-PROFIT_NASH = 0.1133853
-PROFIT_MONOPOLY = 0.1157638717582288
-# TODO: derive more carefully
-MAX_REWARD = 0.12593942529901705
-MIN_REWARD = -0.8668133321973348
-NREWS = 15
+    # Used to calculate the below. TODO: should I use a finer grid to calculate the below?
+min_price_tmp = 0
+price_range_tmp = 2
 
-ECON_PARAMS = np.array([C, A, A0, MU, MIN_PRICE, price_range,
-                        PROFIT_NASH, PROFIT_MONOPOLY, MAX_REWARD, MIN_REWARD,
-                        NREWS])
-
-AI = A
-AJ = A
-
-def profit_n(action_n, c, ai, aj, a0, mu, price_range, min_price):
+def profit_n(action_n, nA, c, ai, aj, a0, mu, price_range, min_price):
     '''
     profit_n gives profits in the market after taking prices as argument
     INPUT
@@ -60,7 +48,7 @@ def profit_n(action_n, c, ai, aj, a0, mu, price_range, min_price):
     a = np.array([ai, aj])
     a_not = np.flip(a) # to obtain the other firm's a
       
-    p = (price_range * action_n/M) + min_price 
+    p = (price_range * action_n/nA) + min_price # minus 1 ? was a comment I left in the other file
     p_not = np.flip(p) # to obtain the other firm's p
     num = np.exp((a - p)/mu)
     denom = np.exp((a - p)/(mu)) + np.exp((a_not - p_not)/(mu)) + np.exp(a0/mu)
@@ -68,6 +56,42 @@ def profit_n(action_n, c, ai, aj, a0, mu, price_range, min_price):
           
     profit = quantity_n * (p-c)
     return(profit)
+
+
+profits = np.zeros((nA, nA))
+
+for p1 in range(nA):
+    for p2 in range(nA):
+        profits[p1][p2] = profit_n(np.array([p1,p2]), nA, C, A, A, A0, MU, price_range_tmp, min_price_tmp)[0]
+
+best_response = np.zeros((nA))
+for p2 in range(nA):
+    best_response[p2] = np.argmax(profits[:, p2])
+
+best_response = np.vstack((best_response, np.arange(nA))).transpose()
+
+Nash = best_response[:,0] == best_response[:,1]
+
+NASH_ACTION = np.argmax(Nash)
+NASH_PRICE = (price_range_tmp * NASH_ACTION/(nA-1)) + min_price_tmp
+NASH_PROFIT = profit_n(np.array((NASH_ACTION, NASH_ACTION)), nA, C, A, A, A0, MU, price_range_tmp, min_price_tmp)
+MIN_PROFIT = np.min(profits)
+MAX_PROFIT = np.max(profits)
+MAX_ACTION = np.argmax(best_response, axis = 0)[0] # highest action ever rational to take
+MAX_PRICE = (price_range_tmp * MAX_ACTION/(nA-1)) + min_price_tmp # Circular?
+MONOPOLY_PROFIT = np.max(np.diag(profits)) # max profit w. constraint there's 1 price
+# MONOPOLY PRICE
+#PROFIT_NASH = 0.1133853
+#PROFIT_MONOPOLY = 0.1157638717582288
+
+MIN_PRICE = 0.9 * NASH_PRICE
+MAX_PRICE = 1.1 * PRICE_MONOPOLY
+price_range = MAX_PRICE- MIN_PRICE
+NREWS = 15
+
+ECON_PARAMS = np.array([C, A, A0, MU, MIN_PRICE, price_range,
+                        PROFIT_NASH, MONOPOLY_PROFIT, MAX_PROFIT, MIN_PROFIT,
+                        NREWS])
     
 def avg_profit_gain(avg_profit):
     '''
@@ -77,16 +101,17 @@ def avg_profit_gain(avg_profit):
     OUTPUT
     apg.............normalised value of the scalar
     '''
-    apg = (avg_profit - PROFIT_NASH) / (PROFIT_MONOPOLY - PROFIT_NASH)
+    apg = (avg_profit - NASH_PROFIT) / (MONOPOLY_PROFIT - NASH_PROFIT)
     return apg
 
 def rew_to_int(reward):
-    rewrange = MAX_REWARD - MIN_REWARD
-    rewint = np.round(NREWS * (reward-MIN_REWARD)/rewrange).astype(int)
+    rewrange = MAX_PROFIT - MIN_PROFIT
+    rewint = np.round(NREWS * (reward-MIN_PROFIT)/rewrange).astype(int)
     return(rewint)
 
-def to_s(act, reward): # TODO: hardcoded 15, 
+def to_s(act, reward):
     '''
 
     '''
     return(act*NREWS + reward)
+
